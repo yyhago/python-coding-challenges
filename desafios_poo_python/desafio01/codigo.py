7fe0f7266e2ce00cb78104966d9edc55e3b1a911
yyhago <yhago.felipe.teles@gmail.com>
2025-04-07 10:02:09 -0300

    Update: desafio 01 - Melhoria em código | poo

--- desafios_poo_python/desafio01/codigo.py
+++ desafios_poo_python/desafio01/codigo.py
@@ -2,7 +2,6 @@ class ContaBancaria:
     def __init__(self, titular, saldo):
         self.titular = titular
         self.saldo = saldo
-        saldo = 0
 
     def depositar(self, valor):
         self.valor = valor
@@ -13,12 +12,34 @@ class ContaBancaria:
         self.valor = valor
         self.saldo -= valor
         print(f"Foi removido R$ {self.valor}, em sua conta.")
+        print(f"O valor atual em sua conta é de R$ {self.saldo}")
 
     def __str__(self):
         return (f"Titular: {self.titular} | Saldo: R$ {self.saldo}")
 
-c1 = ContaBancaria("Yhago", 100)
-print(c1)
-c1.depositar(50)
-c1.sacar(30)
-print(c1)
+
+titular = str(input("Digite seu nome:"))
+saldo = 0
+
+print(f"Olá {titular}, seja bem vindo!")
+c1 = ContaBancaria(titular,saldo)
+
+while True:
+    print(f"Saldo R$ {c1.saldo}")
+    opcao = int(input("1 - Depositar | 2 - Sacar | 3 - Sair: "))
+
+    if opcao == 1:
+        valor = float(input("Digite o valor que deseja depositar: "))
+        c1.depositar(valor)
+
+    if opcao == 2:
+        valor = float(input("Digite o valor que deseja sacar: "))
+        if valor > c1.saldo:
+            print("Você não tem saldo suficiente para este saque!")
+        else:
+            c1.sacar(valor)
+    
+    if opcao == 3:
+        print("Encerrou!")
+        print(f"Titular: {titular} | Saldo: R$ {c1.saldo}")
+        break
